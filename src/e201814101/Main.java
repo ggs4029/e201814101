fc3f7e9feff524d1facc80a795e4f14a81c2f8d9
ggs4029 <ggs65429@gmail.com>
2022-09-26 17:29:40 +0900

    Main.java 수정 #4

--- src/e201814101/Main.java
+++ src/e201814101/Main.java
@@ -1,9 +1,14 @@
 package e201814101;
 
 public class Main {
+
+	static void say() { 
+		System.out.println("main"); 
+	} 
 	
-	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.say("hello world");
+	public static void main(String[] args) { 
+		say(); 
+		Hello hello = new Hello(); 
+		hello.say("hello world"); 
 	}
 }
