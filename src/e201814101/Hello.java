0601ad72f4000bee941b297c1d0cc927f2d18a6b
ggs4029 <ggs65429@gmail.com>
2022-09-26 17:07:14 +0900

    최초 커밋

---
+++ src/e201814101/Hello.java
@@ -0,0 +1,8 @@
+package e201814101;
+
+public class Hello {
+
+	public void say() {
+		System.out.println("hello");
+	}
+}
---
+++ src/e201814101/Main.java
@@ -0,0 +1,7 @@
+package e201814101;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
